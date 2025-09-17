75827f8db19b344a9c89328d7f9d96ff90e90495
Salika155 <carre155@hotmail.com>
2025-09-17 15:04:36 +0200

    jn

---
+++ Codewars_solutions/Codewars_solutions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Codewars_solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
